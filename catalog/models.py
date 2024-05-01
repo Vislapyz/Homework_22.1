6b0a60a260227bde2d9edebe08ddb744a428d784
Mihail <vislapyz@mail.ru>
2024-05-01 19:21:01 +0300

    Задание 2 и 3.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=150, verbose_name='Наименование категории')
+    category_description = models.TextField(verbose_name='Описание категории')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return f'{self.category_name}'
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=150, verbose_name='Наименование продукта')
+    product_description = models.TextField(verbose_name='Описание продукта')
+    imagery = models.ImageField(upload_to='products/', verbose_name='Изображение(превью)', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', related_name="products")
+    cost_product = models.FloatField(verbose_name='Цена за покупку')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return f'{self.product_name} {self.product_description} {self.category} {self.cost_product}'
