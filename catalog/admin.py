00866478722002b32a67892be7433be23f7edcd2
Mihail <vislapyz@mail.ru>
2024-05-02 22:25:03 +0300

    настройте отображение в административной панели

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from catalog.models import Category, Product
+
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'cost_product', 'category',)
+    list_filter = ('category',)
+    search_fields = ('product_name', 'product_description',)
