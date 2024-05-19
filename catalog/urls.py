ffec6c25769c14ee7027bdebdb0a3223be723f2a
Mihail <vislapyz@mail.ru>
2024-05-19 20:39:34 +0300

    ДЗ21.1 задание 1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, product_detail, ProductListView
+from catalog.views import ProductListView, ProductDetailView, ContactsTemplateView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     # path('base/', HomePageView.as_view(), name='base'),
 
-    path('', ProductListView.as_view(), name='products'),
-    path("products/<int:pk>/", product_detail, name='product_detail'),
-    path('contacts/', contacts, name='contacts')
+    path('', ProductListView.as_view(), name='products_list'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('contacts/', ContactsTemplateView.as_view(), name='contacts')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, TemplateView
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
 from catalog.models import Product
 
 
@@ -9,35 +9,33 @@ class ProductListView(ListView):
     model = Product
     template_name = "product_list.html"
 
-# class HomePageView(TemplateView):
-#     template_name = "base.html"
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
 
 
-
-# class HomeView(TemplateView):
-#     template_name = 'catalog/base.html'
-# def home(request):
-#     return render(request, 'base.html')
-#
-# #
-# def products_list(request):
-#     products = Product.objects.all()
-#     context = {"products": products}
-#     return render(request, 'product_list.html', context)
+# def product_detail(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     context = {'product': product}
+#     return render(request, "product_detail.html", context)
 
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
-    return render(request, "product_detail.html", context)
+class ContactsTemplateView(TemplateView):
+    template_name = 'contacts.html'
 
-
-# Задание 19.1
-def contacts(request):
-    if request.method == 'POST':
+    def post(self, request, *args, **kwargs):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f'{name} ({phone}) написал: {message}')
-    return render(request, "contacts.html")
+        print(name, phone, message)
+        return super().get(request, *args, **kwargs)
+# # Задание 19.1
+# def contacts(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         phone = request.POST.get('phone')
+#         message = request.POST.get('message')
+#         print(f'{name} ({phone}) написал: {message}')
+#     return render(request, "contacts.html")
 
