5534fc9a857580ead72bf57d407c37cf752fedf0
Mihail <vislapyz@mail.ru>
2024-05-20 00:38:13 +0300

    ДЗ21.1 задание 2и3

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
 
-from blog.models import Blog
-
-
-@admin.register(Blog)
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'slug', 'date_of_creation', 'is_published', 'view_counter')
-    list_filter = ('date_of_creation',)
-    search_fields = ('title', 'is_published',)
-    prepopulated_fields = {"slug": ("title",)}
+# Register your models here.
